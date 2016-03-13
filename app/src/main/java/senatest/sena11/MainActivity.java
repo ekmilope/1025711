9d424841fa6f7d1513e16f509307911a082171a3
ekmilo <camilinchop@gmail.com>
2016-03-13 18:43:15 -0500

    first commit ui android

---
+++ app/src/main/java/senatest/sena11/MainActivity.java
@@ -0,0 +1,36 @@
+package senatest.sena11;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        //TextView texto = (TextView) findViewById(R.id.text_texto1);
+        //texto.setText("bienvenidos sena");
+
+        setContentView(R.layout.edittext);
+
+        texto = (EditText) findViewById(R.id.text_texto2);
+        texto.getText().toString();
+        texto.setText("bienvenidos sena");
+        Log.d("ETIQUETA", "MENSAJE");
+    }
+
+    public void hacerClick(View view){
+       texto.getText().toString();
+        texto.setText("ud presionó click");
+    }
+
+}
